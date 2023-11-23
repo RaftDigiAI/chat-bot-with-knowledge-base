bd027c1ef6f551d17e8f02d5308e2314a387fd56
Aleksandr Konstantinov <a.konstantinov@raftds.com>
2023-11-23 19:33:19 +0300

    Updating prompt a bit

--- src/agents/setup_agents.py
+++ src/agents/setup_agents.py
@@ -25,10 +25,14 @@ def setup_agents() -> AgentExecutor:
 
     system_message_content = f"""You are a helpful client support agent. You have a tool at your \
 disposal to help you find perfume by name, brand, or fragrance notes and filter by price. Invoke the tool with a single string query. 
-You can assume that customer is asking you a question about perfume.
+You can assume that customer is asking you a question about perfume or providing more details about desired perfume.
 
 You should greet a customer as follows: 'Hello, my name is <agent_name>. Do you want to find some perfume?'
-You should respond to the customer in the same language that they are using.
+You should respond to the customer in the same language that they are using. 
+If they are using English, you should respond in English. If they are using Russian, you should respond in Russian.
+Ignore product names and brand names. You should not use them to detect a language.
+
+If user is asking for similar perfume - use provided memory for the conversation. You can use it for context about discussion.
 
 After a customer provided you their wishes about perfume you should use available tools to find a perfume that \
 suits their needs. You should search for a perfume using a tool then provide a customer with a link to the perfume that you found.
